23c358976aeff17cc2a9b300987aa0dc625e6153
caipengbo <caipengbo@outlook.com>
2017-05-04 23:26:15 +0800

    test commit

--- src/compiler/parser/ProgramParser.java
+++ src/compiler/parser/ProgramParser.java
@@ -5,13 +5,15 @@ import compiler.lexer.Lexer;
 
 /**
  * Title：程序处理
- * Description：
+ * Description： 
  * Created by Myth on 5/4/2017.
  */
 public class ProgramParser {
     //<程序>->[<变量定义部分>] {<自定义函数定义部分>} <主函数>
     //变量声明处理
+	private void var() {
 
+	}
     //自定义函数
     //主函数
     //处理
